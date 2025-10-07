22773bb2afc9edbfc616bb77581f44de63600867
dima <dimazigulin2213@gmail.com>
2025-10-07 20:18:45 +0300

    feat: creating web pages using a templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
-from app import app
+from flask import render_template
+from . import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title="Home", user=user, posts=posts)
