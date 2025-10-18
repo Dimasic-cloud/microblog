c09752f5ff55c08084193dc3f80586dc021b7aec
dima <dimazigulin2213@gmail.com>
2025-10-18 19:38:56 +0300

    исправление ошибки связанной с редактированием профиля пользователя

--- app/forms.py
+++ app/forms.py
@@ -7,13 +7,11 @@ import sqlalchemy as sa
 from app import db
 from app.models import User
 
-
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
-
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -35,8 +33,19 @@ class RegistrationForm(FlaskForm):
         if user is not None:
             raise ValidationError('Please use a different email address.')
 
-
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = db.session.scalar(sa.select(User).where(
+                User.username == self.username.data
+            ))
+            if user is not None:
+                raise ValidationError('Please use a different username.')
