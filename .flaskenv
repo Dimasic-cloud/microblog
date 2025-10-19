bb78987010d7f91e1d0a3899b5ed664242b7d06e
dima <dimazigulin2213@gmail.com>
2025-10-19 20:08:09 +0300

    добавлена огромная и сложная функция, которая позволяет востанавливать пароль с помощью сообщения отправленного на почту. также есть малинькие улучшения и изменения

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,7 @@
 FLASK_APP=microblog.py
 FLASK_DEBUG=0
+MAIL_SERVER=smtp.gmail.com
+MAIL_PORT=587
+MAIL_USE_TLS=true
+MAIL_USERNAME=dimazigulin2213@gmail.com
+MAIL_PASSWORD=nqulclgkdpiwuxfo
--- config.py
+++ config.py
@@ -1,7 +1,14 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    POSTS_PER_PAGE = 25
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'dimazigulin2213@gmail.com')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['dimazigulin2213@gmail.com']
